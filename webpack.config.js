54aeb1fd0980134f6e5830ef4c28268095ca4eb8
Moyinoluwa360 <ogundanajoseph17@gmail.com>
2024-08-13 01:10:48 +0100

    Webpack configuration
    create a starting html template and install all neccessary loaders and plugins and configure on webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    htmlManipulation: "./src/htmlManipulation.js",
+    index: "./src/index.js"
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/template.html'
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   module: {
     rules: [
@@ -12,10 +24,17 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'imgs/[name][ext]'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
     ],
   },
 };
